17ff54bf3b8a97b8de2b32ea2d481a7551cc4082
abhaysachan01 <abhaysachan360@gmail.com>
2025-07-10 13:03:31 +0530

    changes for server

--- server/app.py
+++ server/app.py
@@ -127,6 +127,6 @@ def get_subcategory_weekly_sales():
 def get_profit_weekly():
     return jsonify(load_pivoted_csv(os.path.join(HISTORICAL_RESULTS, "profit_weekly.csv"), "Year-Week"))
 
-
 if __name__ == "__main__":
-    app.run(debug=True, host="localhost", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
